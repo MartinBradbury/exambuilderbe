df57cc9bbef48168c81f2bea133021386e538eba
MartinBradburyPC <you@example.com>
2025-07-25 17:39:17 +0100

    added cors headers to allow frontend

--- exambuilder/settings.py
+++ exambuilder/settings.py
@@ -212,4 +212,6 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://your-netlify-site.netlify.app",
+]
